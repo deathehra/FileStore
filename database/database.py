b8f56104d16ed0a1473646e17a13b31da5ea88a8
Lokesh <23ca1031@mvn.edu.in>
2025-11-14 10:10:54 +0530

    Add original_start parameter to update_verify_status

--- database/database.py
+++ database/database.py
@@ -228,7 +228,7 @@ class Rohit:
         verify = await self.db_verify_status(user_id)
         return verify
 
-    async def update_verify_status(self, user_id, verify_token="", is_verified=False, verified_time=0, link=""):
+    async def update_verify_status(self, user_id, verify_token="", is_verified=False, verified_time=0, original_start="", link=""):
         current = await self.db_verify_status(user_id)
         current['verify_token'] = verify_token
         current['is_verified'] = is_verified
